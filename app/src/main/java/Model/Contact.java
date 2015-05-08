dbc5b7ed56c08bf81a166bc2d2fd511f7ec01823
Kostandin <idinter8@gmail.com>
2015-05-08 09:55:33 +0200

    "New methods for User (get/set methods)"

---
+++ app/src/main/java/Model/Contact.java
@@ -0,0 +1,11 @@
+package Model;
+
+/**
+ * Created by idinter on 08/05/15.
+ */
+public class Contact {
+
+    public Contact(){
+
+    }
+}
--- app/src/main/java/Model/User.java
+++ app/src/main/java/Model/User.java
@@ -1,8 +1,70 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by idinter on 08/05/15.
+ * Created by KM on 08/05/15.
  */
 public class User {
+    private String login = null;
+    private String cel = null;
+    private List<Contact> listeContact= null;
+    private List<Event> listeEvent = null;
+
+    public User(String log,String cel){
+        this.setLogin(log);
+        this.setCel(cel);
+        listeContact = new ArrayList<Contact>();
+        listeEvent = new ArrayList<Event>();
+    }
+
+    /// Public methods ///
+    public void setLogin(String _login){
+        login = _login;
+    }
+    public String getLogin(){
+        return login;
+    }
+    ///
+
+    public void setCel(String _cel){
+        cel = _cel;
+    }
+    public String getCel(){
+        return cel;
+    }
+    ///
+
+    public List<Contact> getListeContact(){
+        return listeContact;
+    }
+    ///
+
+    public List<Event> getListeEvent(){
+        return listeEvent;
+    }
+    ///
+
+    public void addContact(Contact cont){
+        listeContact.add(cont);
+    }
+    ///
+
+    public void addEvent(Event evt){
+        listeEvent.add(evt);
+    }
+    ///
+
+    public void removeContact(Contact cont){
+        listeContact.remove(cont);
+    }
+    ///
+
+    public void removeEvent(Event evt){
+        listeEvent.remove(evt);
+    }
+    ///
+
 
 }
