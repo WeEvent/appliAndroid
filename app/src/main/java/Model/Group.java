93441f96bc7160f435547499c8422c4b2fb1d0fd
Dejon Chloe <chloe.dejon@etu.utc.fr>
2015-05-08 09:58:44 +0200

    Added methods to class Group

--- app/src/main/java/Model/Group.java
+++ app/src/main/java/Model/Group.java
@@ -1,9 +1,40 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by chloedejon on 08/05/2015.
+ * Created by CD on 08/05/2015.
  */
 public class Group {
 
+    private String name;
+    private List<String> contactsList;
 
+    /// Constructor ///
+    public Group(String n){
+        name = n;
+        contactsList = new ArrayList<String>();
+    }
+
+    /// Public methods ///
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String n){
+        name = n;
+    }
+
+    public List<String> getContactsList(){
+        return contactsList;
+    }
+
+    public void addContact(String login){
+        contactsList.add(login);
+    }
+
+    public void removeContact(String login){
+        contactsList.remove(login);
+    }
 }
