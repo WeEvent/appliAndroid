f04c98bf004c4b54a8b6bef49514884abc5881e8
Dejon Chloe <chloe.dejon@etu.utc.fr>
2015-05-24 22:19:45 +0200

    Delete Group

--- app/src/main/java/nf28/weevent/Controller/GroupActivity.java
+++ app/src/main/java/nf28/weevent/Controller/GroupActivity.java
@@ -1,5 +1,7 @@
 package nf28.weevent.Controller;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -7,6 +9,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,6 +27,9 @@ import nf28.weevent.Tools.DataManager;
  * Created by CD on 14/05/2015.
  */
 public class GroupActivity extends ActionBarActivity{
+    String group;
+    ImageButton delete;
+    Button add;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -33,12 +40,18 @@ public class GroupActivity extends ActionBarActivity{
         getSupportActionBar().setHomeButtonEnabled(true);
 
         final TextView groupName = (TextView) this.findViewById(R.id.groupName);
-        String name = getIntent().getStringExtra("group");
-        groupName.setText(name);
+        group = getIntent().getStringExtra("group");
+        groupName.setText(group);
+
+        delete = (ImageButton) this.findViewById(R.id.btn_delete_group);
+        add = (Button) this.findViewById(R.id.btn_add_contact_to_group);
+
+        delete.setOnClickListener(deleteGroupListener);
+        add.setOnClickListener(addContactToGroupListener);
 
         ActionSlideExpandableListView list = (ActionSlideExpandableListView)this.findViewById(R.id.groupList);
 
-        list.setAdapter(buildData(name));
+        list.setAdapter(buildData(group));
         list.setItemActionListener(new ActionSlideExpandableListView.OnActionClickListener() {
             @Override
             public void onClick(View listView, View buttonView, int position) {
@@ -71,6 +84,24 @@ public class GroupActivity extends ActionBarActivity{
                 R.id.text,
                 values);
     }
+
+    Button.OnClickListener addContactToGroupListener
+            = new Button.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+
+        }};
+
+    Button.OnClickListener deleteGroupListener
+            = new Button.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+
+            DataManager.getInstance().removeGroup(group);
+            onBackPressed();
+        }};
 
     @Override
     public boolean onOptionsItemSelected(MenuItem menuItem)
